8705143c31a480e6c6d5a16a8281e0941471b04c
Melihg0 <melih.g.asd@gmail.com>
2024-02-10 23:41:36 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+    { private IApplicantService _applicantservice;
+        public PttManager(IApplicantService applicantservice)
+        {
+            _applicantservice = applicantservice;
+        }
+        public void GiveMask(Person person)
+        {
+            if (_applicantservice.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine("Maske Verilemedi");
+            }
+
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+
+
+Person person1 = new Person();
+person1.FirstName = "Melih";
+person1.LastName = " Gacav";
+person1.DateOfBirthYear = 2000;
+person1.NationalIdentity = 1234567891;
+
+PttManager pttmanager = new PttManager(new PersonManager());
+pttmanager.GiveMask(person1);
+Console.ReadLine();
